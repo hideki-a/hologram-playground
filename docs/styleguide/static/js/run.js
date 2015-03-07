49e0d8f28dc0e2c7e3327640f3be0dab451e1afb
Hideki Abe <hideki.abe.0630@gmail.com>
2015-03-07 15:21:03 +0900

    カスタム要素名を統一

--- docs/styleguide/static/js/run.js
+++ docs/styleguide/static/js/run.js
@@ -2,26 +2,23 @@
 // https://app.codegrid.net/entry/web-components-3
 // http://www.html5rocks.com/ja/tutorials/webcomponents/customelements/
 // http://www.html5rocks.com/ja/tutorials/webcomponents/shadowdom/
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/registerElement
 (function () {
     "use strict";
     var examples = document.querySelectorAll(".codeExample");
+    var XSgOutputExample = document.registerElement("x-sg-output-example");
 
     Array.prototype.forEach.call(examples, function (example, index) {
         var appendTarget = example.querySelector(".exampleOutput");
-        var OutputExample = Object.create(HTMLElement.prototype);
-        var OutputExampleElem;
-        var outputExampleElem = document.createElement("x-sg-output-example-" + index);
+        var customElement;
+        var shadowRoot;
+        var template = example.querySelector(".renderedExampleTmpl");
+        var clone;
 
-        OutputExample.createdCallback = function () {
-            var shadowRoot = this.createShadowRoot();
-            var template = example.querySelector(".renderedExampleTmpl");
-
-            shadowRoot.innerHTML = template.innerHTML;
-        };
-
-        OutputExampleElem = document.registerElement("x-sg-output-example-" + index, {
-            prototype: OutputExample
-        });
-        appendTarget.appendChild(outputExampleElem);
+        appendTarget.appendChild(new XSgOutputExample());
+        customElement = appendTarget.getElementsByTagName("x-sg-output-example")[0];
+        shadowRoot = customElement.createShadowRoot();
+        clone = document.importNode(template.content, true);
+        shadowRoot.appendChild(clone);
     });
 }());
